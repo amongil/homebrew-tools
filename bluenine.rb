04423b0836a1312620a96944cf046904b9ba8cdb
amongil <hello@alvaromongil.com>
2017-11-15 13:08:53 +0100

    Update to version 0.2.0

--- bluenine.rb
+++ bluenine.rb
@@ -1,8 +1,8 @@
 class Bluenine < Formula
     desc "AWS Session Token Manager"
     homepage "https://github.com/amongil/bluenine"
-    url "https://github.com/amongil/bluenine/releases/download/v0.1.2/bluenine-v0.1.2.tar.gz"
-    sha256 "ef869a5bcd2b863cd7ba9168f36528f91b760f6a015c0c141a34b01374c7d4a4"
+    url "https://github.com/amongil/bluenine/releases/download/v0.2.0/bluenine-v0.2.0.tar.gz"
+    sha256 "ce7a4944acc3c29c8a3a0678230ae685047da52d15cf8245c0e2c3413da2e4a8"
   
     bottle :unneeded
   
